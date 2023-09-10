d501a1429af8628beff7a5975b770470494f8881
Johnny <johnlabedzki@gmail.com>
2023-09-10 08:58:34 -0700

    add scripts for smooth scroll and form submit handlers

--- scripts/form-handlers.js
+++ scripts/form-handlers.js
@@ -1,1 +1,21 @@
-/* Your code here */
+const subscribeForm = document.getElementById("subscribe-form");
+const contactForm = document.getElementById("contact-form");
+
+subscribeForm.addEventListener("submit", function (e) {
+  e.preventDefault();
+  const email = this.email.value;
+  alert(
+    `Thank you for subscribing! A welcome email has been sent to ${email}.`
+  );
+  this.reset();
+});
+
+contactForm.addEventListener("submit", function (e) {
+  e.preventDefault();
+  const name = this.name.value;
+  const email = this.email.value;
+  alert(
+    `Thank you for contacting me, ${name}! I will reply to ${email} as soon as I can.`
+  );
+  this.reset();
+});
--- scripts/smooth-scroll.js
+++ scripts/smooth-scroll.js
@@ -1,1 +1,12 @@
-/* Your code here */
+const navLinks = document.querySelectorAll("nav a");
+const backToTopLink = document.getElementById("back-to-top");
+
+[...navLinks, backToTopLink].forEach((link) => {
+  link.addEventListener("click", (e) => {
+    e.preventDefault();
+    const href = link.getAttribute("href");
+    document.querySelector(href).scrollIntoView({
+      behavior: "smooth",
+    });
+  });
+});
